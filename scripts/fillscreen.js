2f37d31fa0a49ff74e36036fcc3ea76f7231596b
Oliver <oliverjgodoy@gmail.com>
2022-12-14 17:21:25 -0300

    query string

--- scripts/fillscreen.js
+++ scripts/fillscreen.js
@@ -1,6 +1,57 @@
 let gamesElement = document.getElementById("games");
 let moreGamesBtn = document.getElementById("moreGamesBtn");
 let gamesInPage = 10;
+
+let all = document.getElementById("active-nav-item")
+let shooter = document.getElementById("shooter")
+let mmo = document.getElementById("mmo")
+let rpg = document.getElementById("rpg")
+let strategy = document.getElementById("strategy")
+let action = document.getElementById("action")
+let sports = document.getElementById("sports")
+let social = document.getElementById("social")
+let fantasy = document.getElementById("fantasy")
+
+const urlParams = new URLSearchParams(window.location.search)
+
+all.addEventListener('click', getAll);
+shooter.addEventListener('click', getShooterParams);
+mmo.addEventListener('click', getMmoParams);
+rpg.addEventListener('click', getRpgParams);
+strategy.addEventListener('click', getStrategyParams);
+action.addEventListener('click', getActionParams);
+sports.addEventListener('click', getSportsParams);
+social.addEventListener('click', getSocialParams);
+fantasy.addEventListener('click', getFantasyParams);
+
+function getAll() {
+	window.location.search = ""
+}
+function getShooterParams() {
+	window.location.search = "?category=shooter"
+}
+function getMmoParams() {
+	window.location.search = "?category=mmo"
+}
+function getRpgParams() {
+	window.location.search = "?category=mmorpg"
+}
+function getStrategyParams() {
+	window.location.search = "?category=strategy"
+}
+function getActionParams() {
+	window.location.search = "?category=action"
+}
+function getSportsParams() {
+	window.location.search = "?category=sports"
+}
+function getSocialParams() {
+	window.location.search = "?category=social"
+}
+function getFantasyParams() {
+	window.location.search = "?category=fantasy"
+}
+
 const options = {
 	method: 'GET',
 	headers: {
@@ -8,7 +59,7 @@ const options = {
 		'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
 	}
 };
-let baseURL = 'https://free-to-play-games-database.p.rapidapi.com/api/games';
+let baseURL = `https://free-to-play-games-database.p.rapidapi.com/api/games?${urlParams}`;
 
 getApiData(gamesInPage);
 
