fdfec76791c447e6d2385873199472c9812e90bc
WisniewskiGabriel <gabwisniewski@gmail.com>
2022-12-15 17:55:13 -0300

    files

--- scripts/fillscreen.js
+++ scripts/fillscreen.js
@@ -1,6 +1,10 @@
 let gamesElement = document.getElementById("games");
 let moreGamesBtn = document.getElementById("moreGamesBtn");
+let allLI = document.getElementsByTagName("li"); 
+let AllBtn = document.getElementsByName("all")[0];
+let DelteBtn = document.getElementById("delete-filter");
 let gamesInPage = 10;
+let params = "games";
 const options = {
 	method: 'GET',
 	headers: {
@@ -8,24 +12,62 @@ const options = {
 		'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
 	}
 };
-let baseURL = 'https://free-to-play-games-database.p.rapidapi.com/api/games';
+let baseURL = 'https://free-to-play-games-database.p.rapidapi.com/api/';
 
-getApiData(gamesInPage);
+AllBtn.addEventListener('click',reloadPage);
+DelteBtn.addEventListener('click',reloadPage);
+
+getApiData(gamesInPage,params);
 
 moreGamesBtn.addEventListener('click',showMoreGames);
 
 function showMoreGames(){
-	gamesInPage = gamesInPage+10;
-	getApiData(gamesInPage);
+	gamesInPage = gamesInPage+12;
+	getApiData(gamesInPage,params);
 }
 
-async function getApiData(gamesInPage){
+function changeParams(inputParams){
+	gamesInPage = 10;
+	params = inputParams;
 
-	let requestJustMade = await fetch(baseURL, options);
+	removeAllActive();
+	
+
+	console.log(inputParams);
+	if(inputParams.includes('category')){
+		elementDir = 'sidebar';
+	}else{
+		elementDir = 'nav';
+	}
+	
+	console.log(elementDir);
+
+	let nameOfClicked = inputParams.match(/(?<==).+/);
+	nameOfClicked = String(nameOfClicked);
+
+	let elementClicked = document.getElementsByName(nameOfClicked);
+	console.log(elementClicked);
+	console.log(nameOfClicked);
+	elementClicked[0].setAttribute('id','active-'+elementDir+'-item');
+
+	getApiData(gamesInPage,params);
+}
+
+async function getApiData(gamesInPage,params){
+
+	console.log(baseURL+params);
+	let requestJustMade = await fetch(baseURL+params, options);
 	let myJson = await requestJustMade.json();
+
+	console.log(myJson);
+
 	let justAddedArticle;
 
-	gamesElement.innerHTML = '';	
+	gamesElement.innerHTML = '';
+	
+	if(myJson.length<gamesInPage){
+		gamesInPage = myJson.length;
+	}
 
 	for(let i=0;i<gamesInPage;i++){
 		let newArticle = elementBuilder(myJson[i]);
@@ -39,4 +81,16 @@ function elementBuilder(gameObj){
 
 	let newArticle = "<article><div id="+gameObj.id+"><img src=/imgs/game-list/bookmark.svg></div><span><h3>"+gameObj.title+"</h3><p>"+gameObj.short_description+"</p></span></article>";
 	return newArticle;
+}
+
+function removeAllActive(){
+	for(let i=0;i<allLI.length;i++){
+		allLI[i].removeAttribute('id','active-sidebar-item');
+		allLI[i].removeAttribute('id','active-nav-item');
+
+	}
+}
+
+function reloadPage(){
+	window.location.reload();
 }
